64cfe3cf351394a60e2bb87f1a9a11cc7e3ddba4
Vinicius Lima <viniciuslimavirtuel@gmail.com>
2022-12-03 12:13:12 -0300

    project convert money

---
+++ README.md
@@ -0,0 +1,13 @@
+# Aplicação concebida com a função de criar um conversor de moedas
+<br>
+<p> ✔️ Técnicas e tecnologias utilizadas: <br> 
+  ⭕ HTML Semântico
+  ⭕ CSS Responsivo
+  ⭕ JavaScript
+</p>
+<br>
+<p> 🔍 Objetivo:  <br>
+  Desenvolver um convertor de moedas (💵 Dólar Americano e Euro 💶) usando as tecnologias estudadas no curso. Assim, aprofundamos os conhecimentos adquiridos. 
+</p>
+
+<br> <br> 
---
+++ index.js
@@ -0,0 +1,47 @@
+/* Projeto feito no dia: 10/11/22, um Dólar fechou na casa dos R$ 5,40 e um Euro R$ 5,49. */
+
+//Selecionar o botão e o select.
+let btn = document.querySelector('button');
+let select = document.querySelector('#currency-select');
+
+//Declarar as constantes do valor das moedas.
+const dolar = 5.40;
+const euro = 5.49;
+
+//Definir a função que irá converter as moedas.
+function convertValues () {
+  const inputReal = document.querySelector('input').value;
+
+  const realText = document.querySelector('#changed-value-rs');
+
+  const dolarText = document.querySelector("#changed-value-us");
+
+  realText.innerHTML = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(inputReal);
+
+
+  if(select.value === 'US$ Dólar Americano'){
+    dolarText.innerHTML = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(inputReal / dolar);
+  } else {
+    dolarText.innerHTML = new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'}).format(inputReal / euro);
+  }
+ 
+}
+
+//Declarar a função que irá trocar o texto e a imagem para Euro.
+changeCurrency = () => {
+  const currencyName = document.querySelector('#currency-name');
+  const currencyImg = document.querySelector('#currency-img');
+
+  if(select.value === '€ Euro'){
+    currencyName.innerHTML = 'Euro:';
+    currencyImg.src = './imgs/euro.svg'
+  } else {
+    currencyName.innerHTML = 'Dólar Americano:';
+    currencyImg.src = './imgs/estados-unidos (1) 1.svg'
+  }
+  convertValues()
+}
+
+//Invocar um evento, ora de click (para o botão), ora de change (para o select).
+btn.addEventListener('click', convertValues)
+select.addEventListener('change',changeCurrency)
